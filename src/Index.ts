166cb5efddcfbc8d3f414672f15db1382d25824e
Vasile-Peste <vasile.peste@protonmail.ch>
2019-10-10 23:45:36 +0200

    Create Vision index point.

---
+++ src/Index.ts
@@ -0,0 +1,11 @@
+// --- Index --- //
+export { Vision, defaultOptions } from "./Vision";
+export { VisionDescriptor } from "./VisionDescriptor";
+export { VisionOptions } from "./VisionOptions";
+
+// --- Entry --- //
+export { MatchedVisionEntry } from "./entry/MatchedVisionEntry";
+export { VisionEntry } from "./entry/VisionEntry";
+export { VisionEntryAuthor } from "./entry/VisionEntryAuthor";
+export { VisionEntryFingerprint } from "./entry/VisionEntryFingerprint";
+export { VisionEntrySet } from "./entry/VisionEntrySet";
