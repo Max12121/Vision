0d073de22ef959d2ba66333c60c4bf3454c9f6e8
Vasile-Peste <vasile.peste@protonmail.ch>
2019-10-30 18:48:59 +0100

    Create function for merging user options with default options.

--- src/utilities/VisionUtilities.ts
+++ src/utilities/VisionUtilities.ts
@@ -5,4 +5,23 @@ export namespace VisionUtilities {
             delete subject[property];
         });
     }
+
+    // Used to merge two options objects.
+    export function mergeOptions (defaultOptions: any, userOptions: any): any {
+        const options: any = {
+            ...defaultOptions,
+            ...userOptions,
+        };
+
+        for (const hash in defaultOptions) {
+            const defaultValue: any = defaultOptions[hash];
+            const userValue: any = userOptions[hash];
+
+            if (defaultValue.constructor === Object && userValue.constructor === Object) {
+                options[hash] = mergeOptions(defaultValue, userValue);
+            }
+        }
+
+        return options;
+    }
 }
