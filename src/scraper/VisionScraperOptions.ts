73c4ed06ad95a78d9b595c5fbeccaa96ebba162b
Vasile-Peste <vasile.peste@protonmail.ch>
2019-10-31 23:33:33 +0100

    Cleanup.

--- src/scraper/VisionScraperOptions.ts
+++ src/scraper/VisionScraperOptions.ts
@@ -1,4 +1,9 @@
+import { IVisionBrowser } from "../browser/IVisionBrowser";
+
 export type VisionScraperOptions = {
+    // Represents the instance of a implementation of the Vision browser. This will be used to visit webpages.
+    browser?: IVisionBrowser;
+
     // Represents the user agent set to the web browser windows.
     userAgent?: string;
 };
